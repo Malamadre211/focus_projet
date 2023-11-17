2e57a5c6aab476c676e6fc3bb0a699868f9d4eb8
malamadre211 <mariangelacurcioartist@gmail.com>
2023-11-17 12:33:41 +0100

    home_ok

---
+++ src/routes/Connexion.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Connexion () {
+
+    return (
+        <>
+            <div className="container">
+            </div>
+        </>
+    )
+}
---
+++ src/routes/Home.tsx
@@ -0,0 +1,101 @@
+import './home.css';
+export function Home () {
+
+    return (
+        <>
+            <div className="header">
+                <div className="navbar">
+                    <div className="menu_burger">
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                    </div>
+                    <div className="logo">LOGO</div>
+                    <div className="avatar">
+                        <img src="/images/profile.png" alt="" width="30px" height="30px" />
+                    </div>
+                </div>
+                <div className="container">
+                <h1>Ma bibliothèque</h1>
+                <h2>Mes histoires générées</h2>
+                <h3>3-5 ans</h3>
+                <div className="slide_histoire">
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                </div>
+                <h3>6-9 ans</h3>
+                <div className="slide_histoire">
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                </div>
+                <h3>10-13 ans</h3>
+                <div className="slide_histoire">
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                </div>
+                <h3>14-16 ans</h3>
+                <div className="slide_histoire">
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="histoire">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                </div>
+                    <h2>Mes e-books importés</h2>
+                <div className="slide_ebook">
+                    <div className="ebook">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="ebook">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                    <div className="ebook">
+                        <p>Titre</p>
+                        <p>Durée</p>
+                    </div>
+                </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
